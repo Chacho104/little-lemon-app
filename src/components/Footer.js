83b71b25af8d7acace88a98388be28ca5be00388
Churchil Owino <chachowino75@gmail.com>
2023-02-08 14:08:58 +0300

    Build the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <section>
+          <h1>Doormat Navigation</h1>
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/orderonline">Order Online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h1>Contact</h1>
+          <ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </section>
+        <section>
+          <h1>Social Media Links</h1>
+          <ul>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+          </ul>
+        </section>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
