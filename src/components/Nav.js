6e89bcb829126de2011a12c9455f38f28fcc14b3
Churchil Owino <chachowino75@gmail.com>
2023-02-08 14:09:45 +0300

    Create and build the Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/orderonline">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
