0313f81ddb9bec81fcc5031d8df6d55bc8e71cac
Churchil Owino <chachowino75@gmail.com>
2023-02-12 09:49:11 +0300

    Set up routing to different pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Fragment } from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
-import Nav from './components/Nav';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import MenuPage from "./pages/Menu";
+import OrderOnlinePage from "./pages/OrderOnline";
+import ReservationsPage from "./pages/Reservations";
+
+// Now it's time to add routing...react-router
+// 1. Install package 2. Define routes/paths/url to support and which components to load for each route (use createBrowserRouter)
+// 3. Activate router and load the route definitions (use RouteProvider)
 
 function App() {
-  return (
-    <Fragment>
-      <Nav />
-      <Header />
-      <Main />
-      <Footer />
-    </Fragment>
-  );
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/about", element: <AboutPage /> },
+    { path: "/menu", element: <MenuPage /> },
+    { path: "/reservations", element: <ReservationsPage /> },
+    { path: "/orderonline", element: <OrderOnlinePage /> },
+    { path: "/login", element: <LoginPage /> },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
