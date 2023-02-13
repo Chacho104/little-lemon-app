725295f577fb3f9edd392d91a603ed4a0e89af99
Churchil Owino <chachowino75@gmail.com>
2023-02-13 07:02:12 +0300

    Build the main's component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,11 @@
-function Main() {
-    return <main>
+import classes from "./Main.module.css";
 
-    </main>
+function Main() {
+  return <main className={classes.main}>
+    <h1>This Week's Special</h1>
+    <h1>Testimonials</h1>
+    <h1>About</h1>
+  </main>;
 }
 
 export default Main;
