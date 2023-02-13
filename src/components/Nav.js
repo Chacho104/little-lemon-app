19392af1fe2cc7be4c0ae7f97438aee1d39fd87d
Churchil Owino <chachowino75@gmail.com>
2023-02-13 07:02:47 +0300

    Add NavLink to nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,76 @@
+import { NavLink, Link } from "react-router-dom";
+import classes from "./Nav.module.css";
+import Logo from "./assets/Logo.svg";
+
 function Nav() {
   return (
-    <nav>
-      <ul>
+    <nav className={classes.nav}>
+      <span>
+        <Link>
+          <img src={Logo} alt="Little Lemon Logo" />
+        </Link>
+      </span>
+      <ul className={classes.navList}>
         <li>
-          <a href="/home">Home</a>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <a href="/about">About</a>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            About
+          </NavLink>
         </li>
         <li>
-          <a href="/menu">Menu</a>
+          <NavLink
+            to="/menu"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Menu
+          </NavLink>
         </li>
         <li>
-          <a href="/reservations">Reservations</a>
+          <NavLink
+            to="/reservations"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Reservations
+          </NavLink>
         </li>
         <li>
-          <a href="/orderonline">Order Online</a>
+          <NavLink
+            to="/orderonline"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Order Online
+          </NavLink>
         </li>
         <li>
-          <a href="/login">Login</a>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
