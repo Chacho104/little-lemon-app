d8cf6c5da99a7eaf681f44e725153d40ecff2ccc
Churchil Owino <chachowino75@gmail.com>
2023-02-13 07:01:28 +0300

    Build header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import classes from "./Header.module.css";
 function Header() {
-  return <header></header>;
+  return (
+    <header className={classes.header}>
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+    </header>
+  );
 }
 
 export default Header;
