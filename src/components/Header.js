45396c4a1535ba7dd50050842b99cdbf07046876
Churchil Owino <chachowino75@gmail.com>
2023-02-14 22:58:30 +0300

    Build the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import classes from "./Header.module.css";
+
+import HeroImg from "./assets/restauranfood.jpg";
+import Button from "./ui/Button";
 function Header() {
   return (
     <header className={classes.header}>
-      <h1>Little Lemon</h1>
-      <h2>Chicago</h2>
+      <div className={classes.hero}>
+        <div className={classes.content}>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Button link="/reservations">Reserve a Table</Button>
+        </div>
+        <div className={classes.image}>
+          <img src={HeroImg} alt="Little Lemon Chef" />
+        </div>
+      </div>
     </header>
   );
 }
