686c90004a08dedad48a1b480f15383e680aa4d5
Churchil Owino <chachowino75@gmail.com>
2023-02-19 08:29:07 +0300

    Create the all meals page

--- src/pages/OrderOnline.js
+++ src/pages/OrderOnline.js
@@ -1,5 +1,7 @@
+import Meals from "../components/Meals/Meals";
+
 function OrderOnlinePage() {
-    return <h1>This is the Order Online Page</h1>
+    return <Meals />
 }
 
 export default OrderOnlinePage;
