44e22ef4d196066945ba286b7fa81d8bb041a836
Churchil Owino <chachowino75@gmail.com>
2023-02-19 08:22:23 +0300

    Move HeroSection and styling to Layout folder

---
+++ src/components/Layout/HeroSection.js
@@ -0,0 +1,27 @@
+import classes from "./HeroSection.module.css";
+
+import HeroImg from "../../assets/restauranfood.jpg";
+import Button from "../UI/Button";
+
+function HeroSection() {
+  return (
+    <main className={classes['hero-section']}>
+      <section className={classes.hero}>
+        <div className={classes.content}>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Button link="/reservations">Reserve a Table</Button>
+        </div>
+        <div className={classes.image}>
+          <img src={HeroImg} alt="Little Lemon Chef" />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default HeroSection;
