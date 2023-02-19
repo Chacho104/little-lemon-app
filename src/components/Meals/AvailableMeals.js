4a47dab21ed3183a5a50a36d6657b0358ef2359d
Churchil Owino <chachowino75@gmail.com>
2023-02-19 08:24:34 +0300

    Create the Available meals component

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem/MealItem";
+
+function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://little-lemon-project-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went horrible wrong!");
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          ...responseData[key],
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      title={meal.title}
+      description={meal.description}
+      price={meal.price}
+      image={meal.image}
+    />
+  ));
+
+  return (
+    <section>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+}
+
+export default AvailableMeals;
