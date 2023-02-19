e1253364e8348424db83a0532ab77b2dac52c205
Churchil Owino <chachowino75@gmail.com>
2023-02-19 08:28:12 +0300

    Refactor the error component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,14 @@
 import { Fragment } from "react";
-import Nav from "../components/Nav";
+import MainNav from "../components/Layout/MainNav";
 
 function ErrorPage() {
   return (
     <Fragment>
-      <Nav />
-      <section className="center-content">
+      <MainNav />
+      <div className="center">
         <h1>An error occurred!</h1>
         <p>Could not find this page!</p>
-      </section>
+      </div>
     </Fragment>
   );
 }
