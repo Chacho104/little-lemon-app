d621d52ba560d3daba0e2799d208c5bc620feff4
Churchil Owino <chachowino75@gmail.com>
2023-02-20 10:06:25 +0300

    Create a dynamic error page element

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,28 @@
 import { Fragment } from "react";
+import { useRouteError } from "react-router-dom";
 import MainNav from "../components/Layout/MainNav";
+import PageContent from "../components/Layout/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page!"
+  }
   return (
     <Fragment>
       <MainNav />
-      <div className="center">
-        <h1>An error occurred!</h1>
-        <p>Could not find this page!</p>
-      </div>
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </Fragment>
   );
 }
