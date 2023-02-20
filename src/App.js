ca4d53f8576e60aa28da42a33489d400e22cfddc
Churchil Owino <chachowino75@gmail.com>
2023-02-20 09:56:37 +0300

    Declutter app.js by adjsuting route definitions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AboutPage from "./pages/About";
-import CustomizeOrderPage from "./pages/CustomizeOrder";
-import DessertsPage from "./pages/Desserts";
+import AllMealsPage from "./pages/AllMeals";
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
-import LunchMealsPage from "./pages/LunchMeals";
-import MainMealsPage from "./pages/MainsMeals";
 import MenuPage from "./pages/Menu";
-import OrderOnlinePage from "./pages/OrderOnline";
-import OrderOnlineRootLayout from "./pages/OrderOnlineRoot";
 import ReservationsPage from "./pages/Reservations";
 import RootLayout from "./pages/Root";
-import SpecialMealsPage from "./pages/SpecialsMeals";
-
-// Now it's time to add routing...react-router
-// 1. Install package 2. Define routes/paths/url to support and which components to load for each route (use createBrowserRouter)
-// 3. Activate router and load the route definitions (use RouteProvider)
 
 function App() {
   const router = createBrowserRouter([
@@ -29,54 +19,7 @@ function App() {
         { path: "about", element: <AboutPage /> },
         { path: "menu", element: <MenuPage /> },
         { path: "reservations", element: <ReservationsPage /> },
-        {
-          path: "orderonline",
-          element: <OrderOnlineRootLayout />,
-          children: [
-            { index: true, element: <OrderOnlinePage /> },
-            {
-              path: "lunch",
-              element: <LunchMealsPage />,
-              children: [
-                {
-                  path: ":mealId",
-                  element: <CustomizeOrderPage />,
-                },
-              ],
-            },
-            {
-              path: "mains",
-              element: <MainMealsPage />,
-              children: [
-                {
-                  path: ":mealId",
-                  element: <CustomizeOrderPage />,
-                },
-              ],
-            },
-            {
-              path: "desserts",
-              element: <DessertsPage />,
-              children: [
-                {
-                  path: ":mealId",
-                  element: <CustomizeOrderPage />,
-                },
-              ],
-            },
-            {
-              path: "specials",
-              element: <SpecialMealsPage />,
-              children: [
-                {
-                  path: ":mealId",
-                  element: <CustomizeOrderPage />,
-                },
-              ],
-            },
-          ],
-        },
-        { path: "orderonline/:mealId", element: <CustomizeOrderPage /> },
+        { path: "orderonline", element: <AllMealsPage /> },
         { path: "login", element: <LoginPage /> },
       ],
     },
