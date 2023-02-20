8b58edd92171293939b455984f0ec88e7979b0c3
Churchil Owino <chachowino75@gmail.com>
2023-02-20 10:03:37 +0300

    Create the meal item component boilerplate

---
+++ src/components/Meals/Layout/MealItem.js
@@ -0,0 +1,10 @@
+function MealItem({ meal }) {
+  return (
+    <section>
+      <h1>{meal.title}</h1>
+      <p>Time to customize your order and add to basket!</p>
+    </section>
+  );
+}
+
+export default MealItem;
