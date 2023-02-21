61dcfddf55af04bccf416bca4285faab304d6662
Churchil Owino <chachowino75@gmail.com>
2023-02-21 22:16:17 +0300

    Import and use meal item form

--- src/components/Meals/Layout/MealItem.js
+++ src/components/Meals/Layout/MealItem.js
@@ -1,7 +1,8 @@
 import SecondaryBtn from "../../UI/SecondaryBtn";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
-function MealItem({ meal }) {
+function MealItem({ meal, toppings }) {
   return (
     <section>
       <div className={classes["meal-item"]}>
@@ -11,7 +12,10 @@ function MealItem({ meal }) {
         <div className={classes.details}>
           <h2>{meal.title}</h2>
           <p>{meal.description}</p>
-          <SecondaryBtn link={".."}  />
+          <SecondaryBtn link={".."} />
+        </div>
+        <div className={classes.customize}>
+          <MealItemForm additions={toppings} meal={meal} />
         </div>
       </div>
     </section>
