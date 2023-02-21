197bc15a99fa780b674d8255bf3c2488eaa8b39c
Churchil Owino <chachowino75@gmail.com>
2023-02-21 22:14:52 +0300

    Add toppings loader

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import MenuPage from "./pages/Menu";
 import ReservationsPage from "./pages/Reservations";
 import RootLayout from "./pages/Root";
 import OrderOnlineRootLayout from "./pages/OrderOnlineRoot";
-import AllMeals, { loader as allMealsLoader} from "./components/Meals/Categories/AllMeals";
+import AllMeals, {
+  loader as allMealsLoader,
+} from "./components/Meals/Categories/AllMeals";
 import BreakfastMeals from "./components/Meals/Categories/BreakfastMeals";
 import LunchMeals from "./components/Meals/Categories/LunchMeals";
 import MainMeals from "./components/Meals/Categories/MainMeals";
 import Desserts from "./components/Meals/Categories/Desserts";
 import SpecialMeals from "./components/Meals/Categories/SpecialMeals";
-import MealDetails from "./components/Meals/Categories/MealDetails";
+import MealDetails, {
+  loader as toppingsLoader,
+} from "./components/Meals/Categories/MealDetails";
 
 function App() {
   const router = createBrowserRouter([
@@ -36,45 +40,68 @@ function App() {
               path: "all-meals",
               children: [
                 { index: true, element: <AllMeals /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
             {
               path: "breakfast",
               children: [
                 { index: true, element: <BreakfastMeals /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
             {
               path: "lunch",
               children: [
                 { index: true, element: <LunchMeals /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
             {
               path: "mains",
               children: [
                 { index: true, element: <MainMeals /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
             {
               path: "desserts",
               children: [
                 { index: true, element: <Desserts /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
             {
               path: "specials",
               children: [
                 { index: true, element: <SpecialMeals /> },
-                { path: ":mealId", element: <MealDetails /> },
+                {
+                  path: ":mealId",
+                  element: <MealDetails />,
+                  loader: toppingsLoader,
+                },
               ],
             },
-
           ],
         },
         { path: "login", element: <LoginPage /> },
