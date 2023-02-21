6682ddeb2ccf61197c7fbfbe0d21e1f9c2f1ce66
Churchil Owino <chachowino75@gmail.com>
2023-02-21 22:15:48 +0300

    Define the toppings loader function

--- src/components/Meals/Categories/MealDetails.js
+++ src/components/Meals/Categories/MealDetails.js
@@ -1,13 +1,36 @@
-import { useParams, useRouteLoaderData } from "react-router-dom";
+import { json, useLoaderData, useParams, useRouteLoaderData } from "react-router-dom";
 import MealItem from "../Layout/MealItem";
 
 function MealDetails() {
+  const loadedToppings = useLoaderData();
+
   const params = useParams();
   const id = params.mealId;
   const loadedMeals = useRouteLoaderData("root");
   const specificMealData = loadedMeals.find((meal) => meal.id === id);
 
-  return <MealItem meal={specificMealData} />;
+  return <MealItem toppings={loadedToppings} meal={specificMealData} />;
 }
 
 export default MealDetails;
+
+export async function loader() {
+  const response = await fetch(
+    "https://little-lemon-project-default-rtdb.firebaseio.com/toppings.json"
+  );
+
+  if (!response.ok) {
+    throw json({ message: "Could not load meal item toppings!" });
+  } else {
+    const respData = await response.json();
+    let loadedToppings = [];
+
+    for (const key in respData) {
+      loadedToppings.push({
+        id: key,
+        ...respData[key],
+      });
+    }
+    return loadedToppings;
+  }
+}
