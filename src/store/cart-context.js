d36db17901f72043d439bfa937c411d237e22e68
Churchil Owino <chachowino75@gmail.com>
2023-03-01 13:51:14 +0300

    Create the basket context

---
+++ src/store/cart-context.js
@@ -0,0 +1,13 @@
+// Step 1: generally create the context...
+// Manage context in some other component e.g CartProvider using useState or useReducer or whatever
+
+import React from "react";
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default CartContext
