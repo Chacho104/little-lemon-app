7aa733554787c48c724e5b5c238d7433c28c4763
Churchil Owino <chachowino75@gmail.com>
2023-03-01 13:58:08 +0300

    Remove toppings fetching logic

--- src/components/Meals/Categories/MealDetails.js
+++ src/components/Meals/Categories/MealDetails.js
@@ -1,36 +1,15 @@
-import { json, useLoaderData, useParams, useRouteLoaderData } from "react-router-dom";
+import { useParams, useRouteLoaderData } from "react-router-dom";
 import MealItem from "../Layout/MealItem";
 
 function MealDetails() {
-  const loadedToppings = useLoaderData();
 
   const params = useParams();
-  const id = params.mealId;
   const loadedMeals = useRouteLoaderData("root");
-  const specificMealData = loadedMeals.find((meal) => meal.id === id);
 
-  return <MealItem toppings={loadedToppings} meal={specificMealData} />;
+  const title = params.mealTitle;
+  const specificMealData = loadedMeals.find((meal) => meal.title === title);
+
+  return <MealItem meal={specificMealData} />;
 }
 
 export default MealDetails;
-
-export async function loader() {
-  const response = await fetch(
-    "https://little-lemon-project-default-rtdb.firebaseio.com/toppings.json"
-  );
-
-  if (!response.ok) {
-    throw json({ message: "Could not load meal item toppings!" });
-  } else {
-    const respData = await response.json();
-    let loadedToppings = [];
-
-    for (const key in respData) {
-      loadedToppings.push({
-        id: key,
-        ...respData[key],
-      });
-    }
-    return loadedToppings;
-  }
-}
