e44fadeb8e592907d03490f1715337b261ab8ec8
Churchil Owino <chachowino75@gmail.com>
2023-03-01 13:54:30 +0300

    Create the basket item component

---
+++ src/components/Cart/BasketItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import classes from "./BasketItem.module.css";
+
+const BasketItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <div>
+        <h2>{props.title}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{price}</span>
+          <span className={classes.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default BasketItem;
