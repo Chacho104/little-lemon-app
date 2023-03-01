f6aeecf19fec571b5c5c066a59d20f31b590c3a9
Churchil Owino <chachowino75@gmail.com>
2023-03-01 13:58:46 +0300

    Use context to add meal item to basket

--- src/components/Meals/Layout/MealItem.js
+++ src/components/Meals/Layout/MealItem.js
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 import SecondaryBtn from "../../UI/SecondaryBtn";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
-function MealItem({ meal, toppings }) {
+function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function addToCartHandler(amount) {
+    cartCtx.addItem({
+      id: meal.id,
+      title: meal.title,
+      amount: amount,
+      price: meal.price,
+    });
+  }
   return (
     <section>
       <div className={classes["meal-item"]}>
@@ -15,7 +27,10 @@ function MealItem({ meal, toppings }) {
           <SecondaryBtn link={".."} />
         </div>
         <div className={classes.customize}>
-          <MealItemForm additions={toppings} meal={meal} />
+          <MealItemForm
+            meal={meal}
+            onAddToCart={addToCartHandler}
+          />
         </div>
       </div>
     </section>
