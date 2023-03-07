a8cc584ed7a894183c373ec32de85b2670434ecd
Churchil Owino <chachowino75@gmail.com>
2023-03-07 21:41:03 +0300

    Build new main meals component

---
+++ src/components/Meals/MainMeals.js
@@ -0,0 +1,15 @@
+import { useRouteLoaderData } from "react-router-dom";
+import MealsList from "./MealsList";
+
+function MainMeals() {
+  const meals = useRouteLoaderData("root");
+  const mainMeals = meals.filter((meal) => meal.category === "Mains");
+
+  return (
+    <section>
+      <MealsList meals={mainMeals} />
+    </section>
+  );
+}
+
+export default MainMeals;
