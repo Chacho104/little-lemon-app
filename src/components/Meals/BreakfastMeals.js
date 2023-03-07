422fda881c1c0859bc4cd4d9bd331c9854ca3009
Churchil Owino <chachowino75@gmail.com>
2023-03-07 21:39:42 +0300

    Build new breakfast meals component

---
+++ src/components/Meals/BreakfastMeals.js
@@ -0,0 +1,15 @@
+import { useRouteLoaderData } from "react-router-dom";
+import MealsList from "./MealsList";
+
+function BreakfastMeals() {
+  const meals = useRouteLoaderData("root");
+  const breakfastMeals = meals.filter((meal) => meal.category === "Breakfast");
+
+  return (
+    <section>
+      <MealsList meals={breakfastMeals} />
+    </section>
+  );
+}
+
+export default BreakfastMeals;
