18aea1a68a918ef40c36ae19d5f18d49f6640ffd
Churchil Owino <chachowino75@gmail.com>
2023-03-07 21:41:22 +0300

    Build new meal details component

---
+++ src/components/Meals/MealDetails.js
@@ -0,0 +1,15 @@
+import { useParams, useRouteLoaderData } from "react-router-dom";
+import MealItemDetails from "./MealItemDetails";
+
+function MealDetails() {
+
+  const params = useParams();
+  const allMeals = useRouteLoaderData("root");
+
+  const title = params.mealTitle;
+  const specificMealData = allMeals.find((meal) => meal.title === title);
+
+  return <MealItemDetails meal={specificMealData} />;
+}
+
+export default MealDetails;
