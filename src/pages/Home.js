95a50f0b3fe5b312d6dd90e1a7b601b26dc1ac09
Churchil Owino <chachowino75@gmail.com>
2023-03-09 22:08:31 +0300

    Use specials component in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import HeroSection from "../components/Layout/HeroSection";
+import Specials from "../components/Meals/Specials";
 
 function HomePage() {
   return (
     <Fragment>
       <HeroSection />
+      <div>
+        <Specials />
+      </div>
     </Fragment>
   );
 }
