1091c17ef56c12edb7036487c2f860e64ff89fbf
Churchil Owino <chachowino75@gmail.com>
2023-03-09 22:02:39 +0300

    Create the special meal item component

---
+++ src/components/Meals/SpecialMealItem.js
@@ -0,0 +1,29 @@
+import { MdOutlineDeliveryDining } from "react-icons/md";
+import { Link } from "react-router-dom";
+import classes from "./SpecialMealItem.module.css";
+import Card from "../UI/Card";
+
+function SpecialMealItem(props) {
+  return (
+    <Card>
+      <li className={classes["meal-item"]}>
+        <div className={classes["meal-image"]}>
+          <img src={"/" + props.image} alt={`Delicious ${props.title}`} />
+        </div>
+        <div className={classes.header}>
+          <h3>{props.title}</h3>
+          <p>${props.price.toFixed(2)}</p>
+        </div>
+        <div className={classes.description}>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.action}>
+          <Link to={`/orderonline/${props.category}/${props.title}`} >Order for Delivery</Link>
+          <MdOutlineDeliveryDining />
+        </div>
+      </li>
+    </Card>
+  );
+}
+
+export default SpecialMealItem;
