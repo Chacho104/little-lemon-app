7696ac5019eebcebf4afd592f870db0246810901
Churchil Owino <chachowino75@gmail.com>
2023-03-09 22:07:14 +0300

    Add delivery icon to component

--- src/components/ui/SecondaryBtn.js
+++ src/components/ui/SecondaryBtn.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import classes from "./SecondaryBtn.module.css";
+import { MdOutlineDeliveryDining } from "react-icons/md";
 
 function SecondaryBtn(props) {
   return (
     <div className={classes.delivery}>
-      <p>Delivery in <strong>30 minutes</strong></p>
+      <div className={classes.time}>
+        <MdOutlineDeliveryDining />
+        <p>
+          Delivery in <strong>30 minutes</strong>
+        </p>
+      </div>
       <Link to={props.link}>{props.action}</Link>
     </div>
   );
