7734dde33ab5505477028d053f6b7ca50edcc4d1
Churchil Owino <chachowino75@gmail.com>
2023-03-09 22:10:20 +0300

    Set up meals loader in rootlayout

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { json, Outlet } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
 import MainNav from "../components/Layout/MainNav";
 import CartProvider from "../store/CartProvider";
@@ -16,3 +16,25 @@ function RootLayout() {
 }
 
 export default RootLayout;
+
+export async function loader() {
+  const response = await fetch(
+    "https://little-lemon-project-default-rtdb.firebaseio.com/meals.json"
+  );
+
+  if (!response.ok) {
+    return json({ message: "Could not fetch events!" }, { status: 500 });
+  } else {
+    const resData = await response.json();
+
+    let meals = [];
+
+    for (const key in resData) {
+      meals.push({
+        id: key,
+        ...resData[key],
+      });
+    }
+    return meals;
+  }
+}
