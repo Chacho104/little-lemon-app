401458acadc6035e0d41e8270c22e266febc4407
Churchil Owino <chachowino75@gmail.com>
2023-03-10 17:07:56 +0300

    Build the testimonials list item component

---
+++ src/components/GeneralSections/TestimonialsListItem.js
@@ -0,0 +1,29 @@
+import Card from "../UI/Card";
+import { FaQuoteLeft } from "react-icons/fa";
+import classes from "./TestimonialsListItem.module.css";
+import Rating from "./Rating";
+
+function TestimonialsListItem(props) {
+  return (
+    <Card>
+      <div className={classes.testimonial}>
+        <div className={classes["testimonial-svg"]}>
+          <FaQuoteLeft />
+        </div>
+        <div className={classes["testimonial-content"]}>{props.comment}</div>
+        <div className={classes["testimonial-user"]}>
+          <div>
+            <h4>{props.name}</h4>
+            <p>{props.occupation}</p>
+            <Rating rating={props.rating} />
+          </div>
+          <div>
+            <img src={"/" + props.image} alt={`${props.name}`} />
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default TestimonialsListItem;
