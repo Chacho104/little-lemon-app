1c5c96fd8900a712fa2225b9f6ec8d4739251961
Churchil Owino <chachowino75@gmail.com>
2023-03-10 17:11:17 +0300

    Use the testimonials and about brief components Define a loader function for fetching testimonials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import { Fragment } from "react";
+import { json } from "react-router-dom";
+import AboutBrief from "../components/GeneralSections/AboutBrief";
+import Testimonials from "../components/GeneralSections/Testimonials";
 import HeroSection from "../components/Layout/HeroSection";
 import Specials from "../components/Meals/Specials";
 
@@ -8,9 +11,33 @@ function HomePage() {
       <HeroSection />
       <div>
         <Specials />
+        <Testimonials />
+        <AboutBrief />
       </div>
     </Fragment>
   );
 }
 
 export default HomePage;
+
+export async function loader() {
+  const response = await fetch(
+    "https://little-lemon-project-default-rtdb.firebaseio.com/testimonials.json"
+  );
+  if (!response.ok) {
+    return json({ message: "Could not fetch testimonials" }, { status: 500 });
+  } else {
+    const resData = await response.json();
+
+    let testimonials = [];
+
+    for (const key in resData) {
+      testimonials.push({
+        id: key,
+        ...resData[key],
+      });
+    }
+
+    return testimonials;
+  }
+}
