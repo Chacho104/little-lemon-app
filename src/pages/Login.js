8c8b9d616d2e0dec4a726d1dc664e0ab36c90da0
Churchil Owino <chachowino75@gmail.com>
2023-03-16 18:00:41 +0300

    Properly format login page component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,11 @@ import FormPageBackground from "../components/Layout/FormPageBackground";
 import LoginForm from "../components/UI/Forms/FullForms/LoginForm";
 
 function LoginPage() {
-  return <FormPageBackground>
-    <LoginForm />
-  </FormPageBackground>;
+  return (
+    <FormPageBackground>
+      <LoginForm />
+    </FormPageBackground>
+  );
 }
 
 export default LoginPage;
