448ebfa31c062299d6b0dbb75a714f176086bdec
Churchil Owino <chachowino75@gmail.com>
2023-03-16 17:58:07 +0300

    Create the select input component

---
+++ src/components/ui/Forms/Inputs/SelectInput.js
@@ -0,0 +1,19 @@
+import { useField } from "formik";
+import classes from "./SelectInput.module.css";
+
+function SelectInput({ label, ...props }) {
+  const [field, meta] = useField(props);
+  return (
+    <div className={classes.input}>
+      <div className={classes["select-input"]}>
+        <label htmlFor={props.id || props.name}>{label}</label>
+        <select {...field} {...props} />
+      </div>
+      {meta.touched && meta.error ? (
+        <div className={classes.error}>{meta.error}</div>
+      ) : null}
+    </div>
+  );
+}
+
+export default SelectInput;
