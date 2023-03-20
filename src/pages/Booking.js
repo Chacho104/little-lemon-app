95906cc20534a6a0361fc4bd8f179d8d24264fad
Churchil Owino <chachowino75@gmail.com>
2023-03-20 19:32:01 +0300

    Create the booking page component

---
+++ src/pages/Booking.js
@@ -0,0 +1,12 @@
+import FormPageBackground from "../components/Layout/FormPageBackground";
+import BookingForm from "../components/UI/Forms/Forms/BookingForm";
+
+function BookingPage() {
+  return (
+    <FormPageBackground>
+      <BookingForm />
+    </FormPageBackground>
+  );
+}
+
+export default BookingPage;
