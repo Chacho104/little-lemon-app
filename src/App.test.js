10639431c1e910e03bfdf060effff88d7acfc75d
Churchil Owino <chachowino75@gmail.com>
2023-03-27 15:51:28 +0300

    Delete unnecessary app test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import BookingPage from './pages/Booking';
-
-test('Renders the booking form header', () => {
-  render(<BookingPage />);
-  const linkElement = screen.getByText(/reserve a table/i);
-  expect(linkElement).toBeInTheDocument();
-});
