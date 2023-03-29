89dfca3a08f8a1d51ab10920a81a4d461f715c12
Churchil Owino <chachowino75@gmail.com>
2023-03-29 22:39:03 +0300

    Add a section element to component

--- src/components/Layout/FormPageBackground.js
+++ src/components/Layout/FormPageBackground.js
@@ -4,7 +4,7 @@ function FormPageBackground(props) {
   return (
     <div className={classes.hero}>
       <div className={classes.overlay}></div>
-      <div className={classes.content}>{props.children}</div>
+      <section className={classes.content}>{props.children}</section>
     </div>
   );
 }
