e736186ab5d808ec6fcb2e1b798ff32248ca3f41
Churchil Owino <chachowino75@gmail.com>
2023-03-29 22:46:55 +0300

    Change default color of stars

--- src/components/ui/Star.js
+++ src/components/ui/Star.js
@@ -1,7 +1,7 @@
 import { VscStarFull } from "react-icons/vsc";
 
 function Star({ filled }) {
-  const color = filled ? "#f4ce14" : "#ccc";
+  const color = filled ? "#f4ce14" : "#808080";
 
   return (
     <span style={{ color, fontSize: "1.5rem" }}>
