9023086d7d340abe23b48556fe0dd9aa890794b6
Churchil Owino <chachowino75@gmail.com>
2023-03-30 11:34:54 +0300

    Dispatch clear cart action

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -53,6 +53,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -70,11 +74,16 @@ function CartProvider(props) {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
