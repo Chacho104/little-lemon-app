ee213f62721d1b58f8cd762840dd4193f7ecbe17
Churchil Owino <chachowino75@gmail.com>
2023-03-30 11:34:34 +0300

    Define clearCart function in cart-context

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -8,6 +8,7 @@ const CartContext = React.createContext({
   totalAmount: 0,
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
-export default CartContext
+export default CartContext;
