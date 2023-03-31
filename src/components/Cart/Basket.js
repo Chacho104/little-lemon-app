88c65dceec7f7c819a2eed847fcff707b8051cee
Churchil Owino <chachowino75@gmail.com>
2023-03-31 12:37:55 +0300

    Set up different food order feature states

--- src/components/Cart/Basket.js
+++ src/components/Cart/Basket.js
@@ -1,12 +1,15 @@
 import classes from "./Basket.module.css";
 import Button from "../UI/Button";
 import SecondaryBtn from "../UI/SecondaryBtn";
-import { useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import BasketItem from "./BasketItem";
 import Input from "../UI/Input";
+import CheckoutForm from "../UI/Forms/Forms/CheckoutForm";
 
 function Basket() {
+  const [checkoutForm, setCheckOutForm] = useState(false);
+  const [basket, setBasket] = useState(true);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -25,6 +28,21 @@ function Basket() {
     cartCtx.clearCart();
   }
 
+  function confirmOrderHandler() {
+    setBasket(false);
+    setCheckOutForm(true);
+  }
+
+  function rethinkOrderHandler() {
+    setBasket(true);
+    setCheckOutForm(false);
+  }
+
+  function orderSuccessfulHandler() {
+    setCheckOutForm(false);
+    setBasket(true);
+  }
+
   const basketItems = (
     <ul>
       {cartCtx.items.map((item) => (
@@ -41,34 +59,50 @@ function Basket() {
   );
 
   return (
-    <section className={classes.basket}>
-      <div>
-        {hasItems && <SecondaryBtn link={"/menu"} action={"Add More"} />}
-      </div>
-      {hasItems && (
-        <div>
-          <h3>Cutlery</h3>
-          <Input
-            label="Help reduce plastic waste. Only ask for cutlery if you need it."
-            input={{ type: "checkbox" }}
-          />
-        </div>
+    <Fragment>
+      {basket && (
+        <section className={classes.basket}>
+          <div>
+            {hasItems && <SecondaryBtn link={"/menu"} action={"Add More"} />}
+          </div>
+          {hasItems && (
+            <div>
+              <h3>Cutlery</h3>
+              <Input
+                label="Help reduce plastic waste. Only ask for cutlery if you need it."
+                input={{ type: "checkbox" }}
+              />
+            </div>
+          )}
+          {hasItems && <h1>Order Summary</h1>}
+          <div className={classes["basket-items"]}>{basketItems}</div>
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span className={classes["final-price"]}>{totalAmount}</span>
+          </div>
+          <div className={classes.actions}>
+            <div className={classes.close}>
+              {hasItems && (
+                <Button onClick={clearCartItemsHandler}>Cancel</Button>
+              )}
+            </div>
+            <div className={classes.proceed}>
+              {hasItems && <Button onClick={confirmOrderHandler}>Order</Button>}
+              {hasItems && checkoutForm}
+            </div>
+          </div>
+        </section>
       )}
-      {hasItems && <h1>Order Summary</h1>}
-      <div className={classes["basket-items"]}>{basketItems}</div>
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <div className={classes.close}>
-          {hasItems && <Button onClick={clearCartItemsHandler}>Cancel</Button>}
-        </div>
-        <div className={classes.proceed}>
-          {hasItems && <Button link="/checkout">Order</Button>}
-        </div>
-      </div>
-    </section>
+      {checkoutForm && (
+        <section className={classes.checkout}>
+          <CheckoutForm
+            totalAmount={totalAmount}
+            rethink={rethinkOrderHandler}
+            orderSuccessful={orderSuccessfulHandler}
+          />
+        </section>
+      )}
+    </Fragment>
   );
 }
 
