188bb36529157bab3f88a0451f99e73d4fd97eb1
Churchil Owino <chachowino75@gmail.com>
2023-03-31 14:43:22 +0300

    Remove h1 element from component

--- src/components/ui/Forms/Forms/FormIntro.js
+++ src/components/ui/Forms/Forms/FormIntro.js
@@ -2,7 +2,6 @@ import classes from "./FormIntro.module.css";
 function FormIntro(props) {
   return (
     <div className={classes.intro}>
-      <h1>{props.title}</h1>
       <p>{props.message}</p>
     </div>
   );
