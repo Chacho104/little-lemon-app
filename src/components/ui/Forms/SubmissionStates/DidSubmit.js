5a8596722752440215225463e484c29545de60e7
Churchil Owino <chachowino75@gmail.com>
2023-03-31 17:17:48 +0300

    Remove unnecessary div

--- src/components/ui/Forms/SubmissionStates/DidSubmit.js
+++ src/components/ui/Forms/SubmissionStates/DidSubmit.js
@@ -2,15 +2,13 @@ import Button from "../../Button";
 
 function DidSubmit(props) {
   return (
-    <section>
-      <div className="booking">
+    <section className="booking">
         <h2>Congratulations!</h2>
         <p>{props.message}</p>
         <p>{props.regards}</p>
         <div className="action">
           <Button onClick={props.onClick}>Close</Button>
         </div>
-      </div>
     </section>
   );
 }
