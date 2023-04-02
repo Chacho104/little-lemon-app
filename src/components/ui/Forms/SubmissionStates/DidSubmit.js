69deb233193aba3a987a12d9230c076c057433a9
Churchil Owino <chachowino75@gmail.com>
2023-04-02 22:13:59 +0300

    Add scroll restoration feature

--- src/components/ui/Forms/SubmissionStates/DidSubmit.js
+++ src/components/ui/Forms/SubmissionStates/DidSubmit.js
@@ -1,15 +1,20 @@
+import { Fragment } from "react";
 import Button from "../../Button";
+import { ScrollRestoration } from "react-router-dom";
 
 function DidSubmit(props) {
   return (
-    <section className="booking">
+    <Fragment>
+      <ScrollRestoration />
+      <section className="booking">
         <h2>Congratulations!</h2>
         <p>{props.message}</p>
         <p>{props.regards}</p>
         <div className="action">
           <Button onClick={props.onClick}>Close</Button>
         </div>
-    </section>
+      </section>
+    </Fragment>
   );
 }
 
