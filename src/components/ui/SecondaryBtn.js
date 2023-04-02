78ef7072190bd5b7ed403b041e76ed913e60bb0e
Churchil Owino <chachowino75@gmail.com>
2023-04-02 21:51:56 +0300

    Make btn resuable in props and no props cases

--- src/components/ui/SecondaryBtn.js
+++ src/components/ui/SecondaryBtn.js
@@ -3,19 +3,31 @@ import classes from "./SecondaryBtn.module.css";
 import { MdOutlineDeliveryDining } from "react-icons/md";
 
 function SecondaryBtn(props) {
-  return (
-    <div className={classes.delivery}>
-      <div className={classes.time}>
-        <MdOutlineDeliveryDining />
-        <p>
-          Delivery in <strong>30 minutes</strong>
-        </p>
+  if (props.link) {
+    return (
+      <div className={classes.delivery}>
+        <div className={classes.time}>
+          <MdOutlineDeliveryDining />
+          <p>
+            Delivery in <strong>30 minutes</strong>
+          </p>
+        </div>
+        <Link to={props.link} preventScrollReset={true}>
+          {props.action}
+        </Link>
       </div>
-      <Link to={props.link} preventScrollReset={true}>
-        {props.action}
-      </Link>
-    </div>
-  );
+    );
+  } else
+    return (
+      <div className={classes.delivery}>
+        <div className={classes['time-only']}>
+          <MdOutlineDeliveryDining />
+          <p>
+            Delivery in <strong>30 minutes</strong>
+          </p>
+        </div>
+      </div>
+    );
 }
 
 export default SecondaryBtn;
