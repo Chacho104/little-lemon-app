956929ee9f3ce5a7264112cd9709cb4aa6b012ca
Churchil Owino <chachowino75@gmail.com>
2023-04-02 21:55:11 +0300

    Create the back button div

--- src/components/Meals/MealItemDetails.js
+++ src/components/Meals/MealItemDetails.js
@@ -3,6 +3,8 @@ import CartContext from "../../store/cart-context";
 import SecondaryBtn from "../UI/SecondaryBtn";
 import classes from "./MealItemDetails.module.css";
 import MealItemForm from "./MealItemForm";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function MealItemDetails({ meal }) {
   const cartCtx = useContext(CartContext);
@@ -15,22 +17,23 @@ function MealItemDetails({ meal }) {
       price: meal.price,
     });
   }
+
   return (
     <section>
       <div className={classes["meal-item"]}>
-        <div>
+        <div className={classes.image}>
           <img src={"/" + meal.image} alt={`Delicious ${meal.title}`} />
+          <Link className={classes.backbtn} to={".."}>
+            <BsArrowLeft />
+          </Link>
         </div>
         <div className={classes.details}>
           <h2>{meal.title}</h2>
           <p>{meal.description}</p>
-          <SecondaryBtn link={".."} action={"Change"} />
+          <SecondaryBtn />
         </div>
         <div className={classes.customize}>
-          <MealItemForm
-            id={meal.id}
-            onAddToCart={addToCartHandler}
-          />
+          <MealItemForm id={meal.id} onAddToCart={addToCartHandler} />
         </div>
       </div>
     </section>
