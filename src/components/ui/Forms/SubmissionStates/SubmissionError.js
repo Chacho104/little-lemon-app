faa60e2290c08f912956ab6676a67f5e94e9a701
Churchil Owino <chachowino75@gmail.com>
2023-04-02 22:13:21 +0300

    Add scroll restoration to page

--- src/components/ui/Forms/SubmissionStates/SubmissionError.js
+++ src/components/ui/Forms/SubmissionStates/SubmissionError.js
@@ -1,11 +1,17 @@
+import { Fragment } from "react";
+import { ScrollRestoration } from "react-router-dom";
+
 function SubmissionError(props) {
   return (
-    <section className="booking">
-      <p>{props.message}</p>
-      <p className="refresh" onClick={props.onClick}>
-        Please try again.
-      </p>
-    </section>
+    <Fragment>
+      <ScrollRestoration />
+      <section className="booking">
+        <p>{props.message}</p>
+        <p className="refresh" onClick={props.onClick}>
+          Please try again.
+        </p>
+      </section>
+    </Fragment>
   );
 }
 
