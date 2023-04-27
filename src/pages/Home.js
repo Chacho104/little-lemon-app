847fa88d26de5b337a3a05f48986d68894ab9bc4
Churchil Owino <chachowino75@gmail.com>
2023-04-27 14:43:43 +0300

    Replace fetch with custom fetch function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
-import { json } from "react-router-dom";
 import AboutBrief from "../components/GeneralSections/AboutBrief";
 import Testimonials from "../components/GeneralSections/Testimonials";
 import MainHeroSection from "../components/Layout/MainHeroSection";
 import Specials from "../components/Meals/Specials";
+import { getAlLTestimonials } from "../dummy-db";
 
 function HomePage() {
   return (
@@ -21,23 +21,7 @@ function HomePage() {
 export default HomePage;
 
 export async function loader() {
-  const response = await fetch(
-    "https://little-lemon-complete-default-rtdb.firebaseio.com/testimonials.json"
-  );
-  if (!response.ok) {
-    return json({ message: "Could not fetch testimonials" }, { status: 500 });
-  } else {
-    const resData = await response.json();
+  const testimonials = getAlLTestimonials();
 
-    let testimonials = [];
-
-    for (const key in resData) {
-      testimonials.push({
-        id: key,
-        ...resData[key],
-      });
-    }
-
-    return testimonials;
-  }
+  return testimonials;
 }
